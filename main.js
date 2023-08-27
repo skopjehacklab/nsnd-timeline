f083ffe2813bc6cd810da692c863c5f75c67ca02
Дамјан Георгиевски <gdamjan@gmail.com>
2023-08-27 20:08:10 +0200

    new Timeline

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-import { Timeline } from '@knight-lab/timelinejs';
-import '@knight-lab/timelinejs/dist/css/timeline.css';
+import { Timeline } from "@knight-lab/timelinejs";
+import "@knight-lab/timelinejs/dist/css/timeline.css";
 
-document.querySelector('#app').textContent = `TODO!`;
+const gsheetId = "2PACX-1vSVkQ1Cpo4gcIwWGFlWg1oYkNizkVuECcO7q9BaKuhA_eLyH6Z_oHkVKet6udyddf6OQg3X_tiLUGj2";
+const url = `https://docs.google.com/spreadsheets/d/e/${gsheetId}/pubhtml`;
+
+const _timeline = new Timeline("timeline-embed", url);
