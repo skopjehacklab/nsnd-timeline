9dfc6f9de57aa764b8113a790c7f931bb996d8e4
Дамјан Георгиевски <gdamjan@gmail.com>
2023-08-27 18:07:29 +0200

    vite, yarn and timelinejs

---
+++ README.md
@@ -0,0 +1,11 @@
+## Quick Start
+```
+yarn
+yarn dev
+```
+
+## Production
+```
+yarn build
+rsync -av dist/ …
+```
---
+++ main.js
@@ -0,0 +1,4 @@
+import { Timeline } from '@knight-lab/timelinejs';
+import '@knight-lab/timelinejs/dist/css/timeline.css';
+
+document.querySelector('#app').textContent = `TODO!`;
